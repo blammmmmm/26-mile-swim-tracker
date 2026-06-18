8619e2fdf5ac86a3bddf1dd2ee3c554a2e6ca522
Ozzy <164962913+blammmmmm@users.noreply.github.com>
2026-06-18 13:44:34 -0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase config for 26-mile swim tracker
+export const firebaseConfig = {
+  apiKey: "AIzaSyBQHMGVqZ03rRy4JPyH432ke_2uBw5h4SE",
+  authDomain: "mile-stream-tracker.firebaseapp.com",
+  databaseURL: "https://mile-stream-tracker-default-rtdb.firebaseio.com",
+  projectId: "mile-stream-tracker",
+  storageBucket: "mile-stream-tracker.firebasestorage.app",
+  messagingSenderId: "450288596554",
+  appId: "1:450288596554:web:d14563f9558e0b6506d8b3",
+  measurementId: "G-RGME0SNTM2"
+};
+
+export const TRACKER_PATH = "swimTracker/current";
+export const TOTAL_MILES = 26;
